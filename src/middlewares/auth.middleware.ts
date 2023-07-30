67a217f2f5813331b7d34448b6aa9cfe9217ff0c
fominykhartur <fominykhartur+GitHub@gmail.com>
2023-07-31 01:15:20 +0300

    add auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,34 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
+  constructor(private readonly jwtService: JwtService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    if (
+      !(
+        req.headers.authorization &&
+        req.headers.authorization?.split(' ')[0] === 'Bearer'
+      )
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    const Token = req.headers.authorization?.split(' ')[1] || '';
+    let isAuth = true;
+    await this.jwtService.verifyAsync(Token).catch(() => {
+      isAuth = false;
+    });
+
+    if (!isAuth) {
+      throw new UnauthorizedException();
+    }
     next();
   }
 }
